5eec908ea6ce431904030c99b7e58d9f8ff33aee
hc <2752523208@qq.com>
2018-02-18 08:53:15 +0800

    homework

--- templates/forms.py
+++ templates/forms.py
@@ -1,23 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, AnyOf
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, AnyOf, Length, EqualTo, Email
 
 
 class LoginForm(FlaskForm):
     username = StringField(
         validators=[
-            DataRequired(message="用户名不能为空"),
+            DataRequired("用户名不能为空"),
         ],
     )
     password = PasswordField(
         validators=[
-            DataRequired(message="密码不能为空")
+            DataRequired("密码不能为空")
         ],
     )
     identity = SelectField(
         '', choices=[(0, '普通用户'), (1, '管理员')],
         validators=[
-            AnyOf([0, 1], message='请以合法身份登录')
+            AnyOf([0, 1], '请以合法身份登录')
         ],
         render_kw={
             'data-select-like-alignement': 'never'
@@ -25,3 +26,26 @@ class LoginForm(FlaskForm):
         coerce=int,
     )
     submit = SubmitField('')
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        validators=[
+            Length(6,20,'用户名长度应在6-20位之间'),
+        ]
+    )
+    password = PasswordField(
+        validators=[
+            Length(6,20,'密码长度应在6-20位之间'),
+        ]
+    )
+    email=EmailField(
+        validators=[
+            Email('请输入合法邮箱')
+        ]
+    )
+    confirm = PasswordField(
+        validators=[
+            EqualTo(password,'两次密码不一致')
+        ]
+    )
--- views.py
+++ views.py
@@ -1,9 +1,14 @@
+from itertools import chain
+
 from flask import render_template, request, redirect, url_for, flash
 from flask_wtf.csrf import CSRFError
 
 from shuju import app,db
 from templates.forms import LoginForm
 
+POST = 'POST'
+
+GET = 'GET'
 
 
 @app.route('/')
@@ -11,21 +16,34 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=[GET, POST])
 def login():
     form = LoginForm()
-    if request.method == 'POST':
+    if request.method == POST:
         if form.validate_on_submit():
-            if not form.errors:
-                db.users.find_one()
-                flash()
-            return redirect(url_for('home'))
+            d=db.users.find_one({
+                'username':form.username.data,
+                'password':form.password.data,
+                'identity':form.identity.data,
+            })
+            if d:
+                flash('登录成功')
+                return redirect(url_for('home'))
+        else:
+            flash('\\n'.join(chain.from_iterable(form.errors.values())))
     return render_template('login.html', form=form)
 
 
 @app.route('/user/home')
 def home():
     return render_template('home.html')
+
+
+@app.route('/register')
+def register():
+    form = Reg
+    if request.method==GET:
+        return render_template('register.html',form=form)
 
 
 @app.errorhandler(CSRFError)
