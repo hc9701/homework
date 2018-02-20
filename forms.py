532396c51bed1e5124ec0548be98189b1389e8d5
hc <2752523208@qq.com>
2018-02-20 16:48:19 +0800

    homework

--- forms.py
+++ forms.py
@@ -15,7 +15,7 @@ class LoginForm(FlaskForm):
             DataRequired("密码不能为空")
         ],
     )
-    identity = SelectField(
+    is_admin = SelectField(
         '', choices=[(0, '普通用户'), (1, '管理员')],
         validators=[
             AnyOf([0, 1], '请以合法身份登录')
@@ -34,7 +34,7 @@ class RegisterForm(FlaskForm):
             Length(6, 20, '用户名长度应在6-20位之间'),
         ],
         render_kw={
-            "placeholder":"用户名",
+            "placeholder": "用户名",
         }
     )
     password = PasswordField(
@@ -42,7 +42,7 @@ class RegisterForm(FlaskForm):
             Length(6, 20, '密码长度应在6-20位之间'),
         ],
         render_kw={
-            "placeholder":"密码",
+            "placeholder": "密码",
         }
     )
     email = EmailField(
@@ -50,15 +50,60 @@ class RegisterForm(FlaskForm):
             Email('请输入合法邮箱'),
         ],
         render_kw={
-            "placeholder":"邮箱",
+            "placeholder": "邮箱",
         }
     )
     confirm = PasswordField(
         validators=[
-            EqualTo('password',message='两次密码不一致')
+            EqualTo('password', message='两次密码不一致')
         ],
         render_kw={
-            "placeholder":"密码确认"
+            "placeholder": "密码确认"
         }
     )
     submit = SubmitField('立即注册')
+
+
+class UserCenterForm(FlaskForm):
+    username = StringField(
+        label='用户名',
+        validators=[
+            Length(6, 20, '用户名长度应在6-20位之间'),
+        ],
+        render_kw={
+            "placeholder": "用户名",
+            'readonly': True,
+        }
+    )
+    new_password = PasswordField(
+        label='新的密码',
+        validators=[
+            Length(6, 20, '密码长度应在6-20位之间'),
+        ],
+        render_kw={
+            "placeholder": "密码",
+        }
+    )
+    confirm = PasswordField(
+        label='密码确认',
+        validators=[
+            EqualTo('new_password', message='两次密码不一致')
+        ],
+        render_kw={
+            "placeholder": "密码确认"
+        }
+    )
+    email = EmailField(
+        label='邮箱',
+        validators=[
+            Email('请输入合法邮箱'),
+        ],
+        render_kw={
+            "placeholder": "邮箱",
+        }
+    )
+    app1 = StringField(label='你想要关注的APP1')
+    app2 = StringField(label='你想要关注的APP2')
+    word1 = StringField(label='你想要关注的分词1')
+    word2 = StringField(label='你想要关注的分词2')
+
--- views.py
+++ views.py
@@ -4,7 +4,7 @@ from flask import render_template, request, redirect, url_for, flash, session
 from flask_wtf.csrf import CSRFError
 
 from shuju import app, db
-from forms import LoginForm, RegisterForm
+from forms import *
 
 POST = 'POST'
 
@@ -24,13 +24,17 @@ def login():
             d = db.users.find_one({
                 'username': form.username.data,
                 'password': form.password.data,
-                'is_admin': form.identity.data,
+                'is_admin': form.is_admin.data,
             })
             if d:
                 flash('登录成功')
                 del d['_id']
                 session['user'] = d
-                return redirect(url_for('home') if not d['identity'] else url_for(''))
+                if d['is_admin']:
+                    # todo
+                    pass
+                else:
+                    return redirect(url_for('home'))
             else:
                 flash('用户名或密码错误')
         else:
@@ -38,12 +42,7 @@ def login():
     return render_template('login.html', form=form)
 
 
-@app.route('/user/home')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/register',methods=[GET,POST])
+@app.route('/register', methods=[GET, POST])
 def register():
     form = RegisterForm()
     if request.method == POST:
@@ -59,13 +58,69 @@ def register():
                 db.users.insert(user_info)
                 flash('注册成功')
                 return redirect(url_for('user_center'))
-            flash('用户名已存在')
-        flash('\\n'.join(chain.from_iterable(form.errors.values())))
+            else:
+                flash('用户名已存在')
+        else:
+            flash('\\n'.join(chain.from_iterable(form.errors.values())))
     return render_template('register.html', form=form)
 
-@app.route('/user/user_center')
+
+@app.route('/user/user_center', methods=[GET, POST])
 def user_center():
-    return render_template('usercenter.html')
+    form = UserCenterForm()
+    user = session['user']
+
+    if request.method == POST:
+        if form.validate_on_submit():
+            if form.username.data != user['username']:
+                flash('不能修改用户名')
+            else:
+                password = form.new_password.data
+                if user['password'] != password and password != '':
+                    user['password'] = password
+                user['app1'] = form.app1.data
+                user['app2'] = form.app2.data
+                user['word1'] = form.word1.data
+                user['word2'] = form.word2.data
+                db.users.update_one({
+                    'username': user['username'],
+                },
+                    {'$set': user}
+                )
+                flash('更新成功')
+        else:
+            flash('\\n'.join(chain.from_iterable(form.errors.values())))
+    else:
+        user = session['user']
+        form.username.data = user['username']
+        form.new_password.data = ''
+        form.confirm.data = ''
+        form.email.data = user.get('email')
+        form.app1.data = user.get('app1')
+        form.app2.data = user.get('app2')
+        form.word1.data = user.get('word1')
+        form.word2.data = user.get('word2')
+    return render_template('usercenter.html', form=form)
+
+
+@app.route('/user/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/user/download')
+def download():
+    return render_template('APPdownload.html')
+
+
+@app.route('/user/details')
+def detail():
+    return render_template('Details.html')
+
+
+@app.route('/user/score')
+def score():
+    return render_template('score.html')
 
 
 @app.errorhandler(CSRFError)
